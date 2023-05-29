68ea99d5b330e5dc34e187e0ab53d556fc064b08
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-05-29 20:30:09 +0530

    instant tracking added to the whole threejs scene

--- src/index.ts
+++ src/index.ts
@@ -8,250 +8,13 @@ import * as THREE from "three";
 import * as ZapparThree from "@zappar/zappar-threejs";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { FlyControls } from "three/examples/jsm/controls/FlyControls";
+import Stats from "three/examples/jsm/libs/stats.module";
 
-const model = new URL("../assets/waving.glb", import.meta.url).href;
+const model = new URL("../assets/Stadium/Cricket.glb", import.meta.url).href;
 
 import "./index.css";
 
-const init = () => {
-  // Canvas
-  const canvas = document.querySelector("canvas.webgl");
-
-  // Scene
-  const scene = new THREE.Scene();
-
-  /**
-   * Textures
-   */
-  // const textureLoader = new THREE.TextureLoader();
-
-  // const gltfLoader = new GLTFLoader();
-
-  // const gltfLoader = new GLTFLoader(manager);
-
-  // gltfLoader.load(
-  //   model,
-  //   (gltf) => {
-  //     console.log(gltf);
-  //     // Now the model has been loaded, we can add it to our instant_tracker_group
-  //     scene.add(gltf.scene);
-  //   },
-  //   (xhr) => {
-  //     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  //   },
-  //   (err) => {
-  //     console.log("An error ocurred loading the GLTF model", err);
-  //   }
-  // );
-
-  const button_six = document.querySelector(".six");
-  const button_four = document.querySelector(".four");
-
-  // gltfLoader.load("../assets/Stadium/Cricket.glb", (gltf) => {
-  //   gltf.scene.position.set(0, -0.75, 0);
-  //   gltf.scene.scale.set(0.1, 0.1, 0.1);
-  //   scene.add(gltf.scene);
-  // });
-
-  // ==================== Function to create a 3D Sphere ====================
-  const CreateSphere = (x, y, z, color) => {
-    const sphereMesh = new THREE.Mesh(
-      new THREE.SphereGeometry(0.1, 32, 32),
-      new THREE.MeshBasicMaterial({ color: color })
-    );
-    sphereMesh.scale.set(0.1, 0.1, 0.1);
-    sphereMesh.position.set(x, y, z);
-    scene.add(sphereMesh);
-  };
-  // ===================== Logic for marking a boundary ======================
-  CreateSphere(-0.125, 0.02, 0.02, "red");
-  CreateSphere(
-    0.65 * Math.cos(Math.PI / 2) - 0.125,
-    0.09,
-    0.65 * Math.sin(Math.PI / 2) + 0.02,
-    "green"
-  );
-  CreateSphere(
-    0.68 * Math.cos(Math.PI * 1.8) - 0.125,
-    0.09,
-    0.68 * Math.sin(Math.PI * 1.8) + 0.02,
-    "green"
-  );
-  CreateSphere(
-    0.7 * Math.cos(Math.PI * 1.5) - 0.125,
-    0.09,
-    0.7 * Math.sin(Math.PI * 1.5) + 0.02,
-    "green"
-  );
-
-  // =====================  Sleep Function ======================
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  // =====================  Animation of TubeGeometry Function ======================
-  async function animate(points, mesh) {
-    for (let i = 2; i < points.length; i++) {
-      let curve = new THREE.CatmullRomCurve3(points.slice(0, i));
-      mesh.geometry = new THREE.TubeGeometry(curve, 64, 0.007, 8, false);
-      await sleep(25);
-    }
-  }
-
-  // =====================  Function for creating a curve ======================
-  const CreateCurve = (x, y, z, run) => {
-    let v1 = new THREE.Vector3(-0.125, 0.02, 0.02); // pos of batsman
-    let v2 = new THREE.Vector3(x, y, z); // endpoint of the ball
-    let points = [];
-    if (run == 6) {
-      for (let i = 0; i <= 50; i++) {
-        let p = new THREE.Vector3().lerpVectors(v1, v2, i / 50);
-
-        p.y = p.y + 0.2 * Math.sin((Math.PI * i) / 50);
-        points.push(p);
-      }
-      let curve = new THREE.CatmullRomCurve3(points.slice(0, 2));
-      const geometry = new THREE.TubeGeometry(curve, 64, 0.007, 8, false);
-      const material = new THREE.MeshBasicMaterial({ color: "red" });
-      const mesh = new THREE.Mesh(geometry, material);
-      scene.add(mesh);
-      animate(points, mesh);
-    } else if (run == 4) {
-      for (let i = 0; i <= 50; i++) {
-        let p = new THREE.Vector3().lerpVectors(v1, v2, i / 50);
-        p.y = p.y + 0.01 * Math.sin((Math.PI * i) / 50);
-        points.push(p);
-      }
-      console.log(points);
-      let curve = new THREE.CatmullRomCurve3(points.slice(0, 2));
-      const geometry = new THREE.TubeGeometry(curve, 64, 0.007, 8, false);
-      const material = new THREE.MeshBasicMaterial({ color: "blue" });
-      const mesh = new THREE.Mesh(geometry, material);
-      scene.add(mesh);
-      animate(points, mesh);
-    }
-  };
-
-  // =====================  Event Listeners and Calling the CreateCurve Function ======================
-  button_six.addEventListener("click", () => {
-    CreateCurve(
-      0.65 * Math.cos(Math.PI / 2) - 0.125,
-      0.09,
-      0.65 * Math.sin(Math.PI / 2) + 0.02,
-      6
-    );
-    CreateCurve(
-      0.7 * Math.cos(Math.PI * 2.3) - 0.125,
-      0.09,
-      0.7 * Math.sin(Math.PI * 2.3) + 0.02,
-      6
-    );
-  });
-
-  button_four.addEventListener("click", () => {
-    CreateCurve(
-      0.68 * Math.cos(Math.PI * 1.8) - 0.125,
-      0.05,
-      0.68 * Math.sin(Math.PI * 1.8) + 0.02,
-      4
-    );
-    CreateCurve(
-      0.7 * Math.cos(Math.PI * 1.5) - 0.125,
-      0.05,
-      0.7 * Math.sin(Math.PI * 1.5) + 0.02,
-      4
-    );
-  });
-
-  /**
-   * Sizes
-   */
-  const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  };
-
-  window.addEventListener("resize", () => {
-    // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  });
-
-  const light = new THREE.AmbientLight(0x404040); // soft white light
-  light.intensity = 4;
-  light.position.set(0, 2, 0);
-  scene.add(light);
-
-  /**
-   * Camera
-   */
-  // Base camera
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    100
-  );
-  camera.position.set(0, 3, 1);
-  scene.add(camera);
-
-  // Controls
-  const controls = new OrbitControls(camera, canvas);
-  controls.enableDamping = true;
-
-  //
-
-  /**
-   * Renderer
-   */
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true,
-  });
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-  /**
-   * Animate
-   */
-  const clock = new THREE.Clock();
-
-  const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-
-    // shaderMaterial.uniforms.uTime.value = elapsedTime;
-    // Update controls
-    controls.update();
-
-    // Render
-    renderer.render(scene, camera);
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
-  };
-
-  tick();
-};
-
-// zappar part here
-
-// export const loadGLTF = (path: any) => {
-//   return new Promise((resolve, reject) => {
-//     const loader = new GLTFLoader();
-//     loader.load(path, (gltf) => {
-//       resolve(gltf);
-//     });
-//   });
-// };
-// console.log(init);
 // The SDK is supported on many different browsers, but there are some that
 // don't provide camera access. This function detects if the browser is supported
 // For more information on support, check out the readme over at
@@ -272,16 +35,20 @@ if (ZapparThree.browserIncompatible()) {
 // your own loading UI - it's up to you :-)
 const manager = new ZapparThree.LoadingManager();
 
+// ==================== Selectings dom elemets ====================
+const canvas = document.querySelector("canvas.webgl");
+const button_six = document.querySelector(".six");
+const button_four = document.querySelector(".four");
+
 // Construct our ThreeJS renderer and scene as usual
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+});
 const scene = new THREE.Scene();
-document.body.appendChild(renderer.domElement);
+// document.body.appendChild(renderer.domElement);
 
 // As with a normal ThreeJS scene, resize the canvas if the window resizes
-renderer.setSize(window.innerWidth, window.innerHeight);
-window.addEventListener("resize", () => {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
 
 // Create a Zappar camera that we'll use instead of a ThreeJS camera
 const camera = new ZapparThree.Camera();
@@ -313,14 +80,37 @@ const instantTrackerGroup = new ZapparThree.InstantWorldAnchorGroup(
 // Add our instant tracker group into the ThreeJS scene
 scene.add(instantTrackerGroup);
 
+// THREE-JS CODE STARTS HERE
+
+// ==================== Creating custom movement using rayCaster ====================
+
+const arrowHelper = new THREE.ArrowHelper(
+  new THREE.Vector3(),
+  new THREE.Vector3(),
+  0.25,
+  0xffff00
+);
+instantTrackerGroup.add(arrowHelper);
+
+const material = new THREE.MeshNormalMaterial();
+
+const boxGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const coneGeometry = new THREE.ConeGeometry(0.05, 0.2, 8);
+
+const raycaster = new THREE.Raycaster();
+const sceneMeshes: THREE.Object3D[] = [];
+
 // Load a 3D model to place within our group (using ThreeJS's GLTF loader)
 // Pass our loading manager in to ensure the progress bar works correctly
 const gltfLoader = new GLTFLoader(manager);
 
-gltfLoader.load(
+const start = gltfLoader.load(
   model,
   (gltf) => {
     console.log(gltf);
+    gltf.scene.scale.set(0.1, 0.1, 0.1);
+    gltf.scene.position.y = -1;
+
     // Now the model has been loaded, we can add it to our instant_tracker_group
     instantTrackerGroup.add(gltf.scene);
   },
@@ -332,33 +122,249 @@ gltfLoader.load(
   }
 );
 
-// const loader = new GLTFLoader();
-// loader.load(
-//   "../assets/waving.glb",
-//   function (gltf) {
-//     scene.add(gltf.scene);
-//   },
-//   (xhr) => {
-//     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-//   },
-//   (error) => {
-//     console.log(error);
-//   }
-// );
+canvas.addEventListener("dblclick", onDoubleClick, false);
+canvas.addEventListener("mousemove", onMouseMove, false);
+
+const mouse = new THREE.Vector2();
+
+function onMouseMove(event: MouseEvent) {
+  console.log("started");
+  mouse.set(
+    (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+    -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+  );
+
+  console.log(mouse.x, mouse.y);
+  camera.position.x += 1;
+  camera.position.y += 1;
+
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObjects(sceneMeshes, false);
+  console.log(intersects);
+  if (intersects.length > 0) {
+    // console.log(sceneMeshes.length + " " + intersects.length)
+    // console.log(intersects[0])
+    // console.log(intersects[0].object.userData.name + " " + intersects[0].distance + " ")
+    // console.log((intersects[0].face as THREE.Face).normal)
+    // line.position.set(0, 0, 0)
+    // line.lookAt((intersects[0].face as THREE.Face).normal)
+    // line.position.copy(intersects[0].point)
+
+    const n = new THREE.Vector3();
+    n.copy((intersects[0].face as THREE.Face).normal);
+    n.transformDirection(intersects[0].object.matrixWorld);
+
+    arrowHelper.setDirection(n);
+    arrowHelper.position.copy(intersects[0].point);
+  }
+}
+
+function onDoubleClick(event: MouseEvent) {
+  mouse.set(
+    (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+    -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+  );
+  raycaster.setFromCamera(mouse, camera);
+  camera.position.z += 10;
+  camera.position.y += 10;
+
+  const intersects = raycaster.intersectObjects(sceneMeshes, false);
+
+  if (intersects.length > 0) {
+    const n = new THREE.Vector3();
+    n.copy((intersects[0].face as THREE.Face).normal);
+    n.transformDirection(intersects[0].object.matrixWorld);
+
+    // const cube = new THREE.Mesh(boxGeometry, material)
+    const cube = new THREE.Mesh(coneGeometry, material);
+
+    cube.lookAt(n);
+    cube.rotateX(Math.PI / 2);
+    cube.position.copy(intersects[0].point);
+    cube.position.addScaledVector(n, 0.1);
+
+    instantTrackerGroup.add(cube);
+    sceneMeshes.push(cube);
+  }
+}
+
+console.log(sceneMeshes);
+
+// ==================== Function to create a 3D Sphere ====================
+const CreateSphere = (x, y, z, color) => {
+  const sphereMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(0.1, 32, 32),
+    new THREE.MeshBasicMaterial({ color: color })
+  );
+  sphereMesh.scale.set(0.1, 0.1, 0.1);
+  sphereMesh.position.set(x, y, z);
+  instantTrackerGroup.add(sphereMesh);
+};
+
+// ===================== Logic for marking a boundary ======================
+CreateSphere(-0.125, 0.02, 0.02, "red");
+CreateSphere(
+  0.65 * Math.cos(Math.PI / 2) - 0.125,
+  0.09,
+  0.65 * Math.sin(Math.PI / 2) + 0.02,
+  "green"
+);
+CreateSphere(
+  0.68 * Math.cos(Math.PI * 1.8) - 0.125,
+  0.09,
+  0.68 * Math.sin(Math.PI * 1.8) + 0.02,
+  "green"
+);
+CreateSphere(
+  0.7 * Math.cos(Math.PI * 1.5) - 0.125,
+  0.09,
+  0.7 * Math.sin(Math.PI * 1.5) + 0.02,
+  "green"
+);
+
+// =====================  Sleep Function ======================
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// =====================  Animation of TubeGeometry Function ======================
+async function animate(points, mesh) {
+  for (let i = 2; i < points.length; i++) {
+    let curve = new THREE.CatmullRomCurve3(points.slice(0, i));
+    mesh.geometry = new THREE.TubeGeometry(curve, 64, 0.007, 8, false);
+    await sleep(25);
+  }
+}
+
+// =====================  Function for creating a curve ======================
+const CreateCurve = (x, y, z, run) => {
+  let v1 = new THREE.Vector3(-0.125, 0.02, 0.02); // pos of batsman
+  let v2 = new THREE.Vector3(x, y, z); // endpoint of the ball
+  let points = [];
+  if (run == 6) {
+    for (let i = 0; i <= 50; i++) {
+      let p = new THREE.Vector3().lerpVectors(v1, v2, i / 50);
+
+      p.y = p.y + 0.2 * Math.sin((Math.PI * i) / 50);
+      points.push(p);
+    }
+    let curve = new THREE.CatmullRomCurve3(points.slice(0, 2));
+    const geometry = new THREE.TubeGeometry(curve, 64, 0.007, 8, false);
+    const material = new THREE.MeshBasicMaterial({ color: "red" });
+    const mesh = new THREE.Mesh(geometry, material);
+    instantTrackerGroup.add(mesh);
+    animate(points, mesh);
+  } else if (run == 4) {
+    for (let i = 0; i <= 50; i++) {
+      let p = new THREE.Vector3().lerpVectors(v1, v2, i / 50);
+      p.y = p.y + 0.01 * Math.sin((Math.PI * i) / 50);
+      points.push(p);
+    }
+    console.log(points);
+    let curve = new THREE.CatmullRomCurve3(points.slice(0, 2));
+    const geometry = new THREE.TubeGeometry(curve, 64, 0.007, 8, false);
+    const material = new THREE.MeshBasicMaterial({ color: "blue" });
+    const mesh = new THREE.Mesh(geometry, material);
+    instantTrackerGroup.add(mesh);
+    animate(points, mesh);
+  }
+};
+
+// =====================  Event Listeners and Calling the CreateCurve Function ======================
+button_six.addEventListener("click", () => {
+  CreateCurve(
+    0.65 * Math.cos(Math.PI / 2) - 0.125,
+    0.09,
+    0.65 * Math.sin(Math.PI / 2) + 0.02,
+    6
+  );
+  CreateCurve(
+    0.7 * Math.cos(Math.PI * 2.3) - 0.125,
+    0.09,
+    0.7 * Math.sin(Math.PI * 2.3) + 0.02,
+    6
+  );
+});
+
+button_four.addEventListener("click", () => {
+  CreateCurve(
+    0.68 * Math.cos(Math.PI * 1.8) - 0.125,
+    0.05,
+    0.68 * Math.sin(Math.PI * 1.8) + 0.02,
+    4
+  );
+  CreateCurve(
+    0.7 * Math.cos(Math.PI * 1.5) - 0.125,
+    0.05,
+    0.7 * Math.sin(Math.PI * 1.5) + 0.02,
+    4
+  );
+});
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  // camera._updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 // Let's add some lighting, first a directional light above the model pointing down
 const directionalLight = new THREE.DirectionalLight("white", 0.8);
 directionalLight.position.set(0, 5, 0);
 directionalLight.lookAt(0, 0, 0);
 instantTrackerGroup.add(directionalLight);
-const start = init();
-
-instantTrackerGroup.add(start);
 
 // And then a little ambient light to brighten the model up a bit
 const ambientLight = new THREE.AmbientLight("white", 0.4);
 instantTrackerGroup.add(ambientLight);
 
+/**
+ * Camera
+ */
+// Base camera
+// const camera2 = new THREE.PerspectiveCamera(
+//   75,
+//   sizes.width / sizes.height,
+//   0.1,
+//   100
+// );
+camera.position.set(0, 10, 1);
+// scene.add(camera2);
+
+// Controls !!!!! NOT WORKING , ZAPPAR CAMERA NOT USABLE WITH ORBITAL CONTROLS !!!!
+
+// const controls = new OrbitControls(camera, canvas);
+// // controls.autoRotate();
+// controls.enableDamping = true;
+// console.log(controls);
+// controls.update();
+
+//TRYING FLY CONTROLS
+
+// controls = new FlyControls(camera, canvas);
+// controls.movementSpeed = 100;
+// controls.rollSpeed = Math.PI / 24;
+// controls.autoForward = false;
+// controls.dragToLook = true;
+
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// const clock = new THREE.Clock();
 // When the experience loads we'll let the user choose a place in their room for
 // the content to appear using setAnchorPoseFromCameraOffset (see below)
 // The user can confirm the location by tapping on the screen
@@ -370,6 +376,10 @@ placeButton.addEventListener("click", () => {
   placeButton.remove();
 });
 
+console.log(instantTrackerGroup);
+
+const clock = new THREE.Clock();
+let delta = 0;
 // Use a function to render our scene as usual
 function render(): void {
   if (!hasPlaced) {
@@ -383,8 +393,13 @@ function render(): void {
 
   // Draw the ThreeJS scene in the usual way, but using the Zappar camera
   renderer.render(scene, camera);
+  // controls.update(0.01);
+  delta = clock.getDelta();
 
+  // camera.position.y += 1;
+  console.log(camera.position);
   // Call render() again next frame
+
   requestAnimationFrame(render);
 }
 
